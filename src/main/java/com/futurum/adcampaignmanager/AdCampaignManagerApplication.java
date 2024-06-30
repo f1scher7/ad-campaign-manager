7a4b3a53756b03eeb48a589e888cfe87aa1bdbef
f1scher7 <maksym.szylo@gmail.com>
2024-06-30 13:31:30 +0200

    Initial commit

---
+++ src/main/java/com/futurum/adcampaignmanager/AdCampaignManagerApplication.java
@@ -0,0 +1,13 @@
+package com.futurum.adcampaignmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdCampaignManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdCampaignManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/futurum/adcampaignmanager/AdCampaignManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.futurum.adcampaignmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdCampaignManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
