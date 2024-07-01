6a324a4960fbe0825ced5f5085f00b22d569967b
f1scher7 <maksym.szylo@gmail.com>
2024-07-02 00:11:09 +0200

    Added CampaignRepository

---
+++ src/main/java/com/futurum/adcampaignmanager/repositories/CampaignRepository.java
@@ -0,0 +1,12 @@
+package com.futurum.adcampaignmanager.repositories;
+
+import com.futurum.adcampaignmanager.entity.Campaign;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CampaignRepository extends JpaRepository<Campaign, Long> {
+
+    Campaign findByCampaignName(String campaignName);
+
+}
