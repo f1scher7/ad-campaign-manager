bf68ce2850b61d9f310c43b5d2b6237f2d74c984
f1scher7 <maksym.szylo@gmail.com>
2024-07-02 00:09:02 +0200

    Added WEbConfig

---
+++ src/main/java/com/futurum/adcampaignmanager/config/WebConfig.java
@@ -0,0 +1,23 @@
+package com.futurum.adcampaignmanager.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry corsRegistry) {
+                corsRegistry.addMapping("/auth/login")
+                        .allowedOrigins("http://localhost:4200")
+                        .allowedMethods("POST");
+            }
+        };
+    }
+
+}
