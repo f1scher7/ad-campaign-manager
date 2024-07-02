af00fa0fa60b5cac4f8d9728559f93d6ab592640
f1scher7 <maksym.szylo@gmail.com>
2024-07-02 21:53:25 +0200

    Added userCount func

--- src/main/java/com/futurum/adcampaignmanager/repositories/UserRepository.java
+++ src/main/java/com/futurum/adcampaignmanager/repositories/UserRepository.java
@@ -2,6 +2,7 @@ package com.futurum.adcampaignmanager.repositories;
 
 import com.futurum.adcampaignmanager.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -10,5 +11,8 @@ public interface UserRepository extends JpaRepository<User,Long> {
     User findByUsername(String username);
 
     User findByEmail(String email);
+
+    @Query("SELECT COUNT(u) FROM User u WHERE u.role = 'USER'")
+    long countUsersWithRoleUser();
 
 }
