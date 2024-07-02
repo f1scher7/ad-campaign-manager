95fa64ce8dce6195449687aecd862cc34419586d
f1scher7 <maksym.szylo@gmail.com>
2024-07-02 21:51:26 +0200

    Added routes for AdminDashboardComponent and RegisterComponent

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,7 +1,13 @@
 import { Routes } from '@angular/router';
-import { LoginComponent } from "./login/login.component";
+import { LoginComponent } from "./components/auth/login/login.component";
+import { RegisterComponent } from "./components/auth/register/register.component";
+import { AdminDashboardComponent } from "./components/admin/admin-dashboard/admin-dashboard.component";
+import { AuthGuard } from "./guards/auth.guard";
 
 export const routes: Routes = [
+    { path: '', redirectTo: 'auth/login', pathMatch: "full" },
     { path: 'auth/login', component: LoginComponent },
-    { path: '**', redirectTo: 'auth/login' }
+    { path: 'auth/register', component: RegisterComponent },
+    { path: 'adashboard', component: AdminDashboardComponent, canActivate: [AuthGuard], data: { role: 'ADMIN' } },
+    { path: '**', redirectTo: 'auth/login' },
 ];
