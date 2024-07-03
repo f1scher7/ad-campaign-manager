20b933e97ee4d7c7ee3e86f254da2397e57cd125
f1scher7 <maksym.szylo@gmail.com>
2024-07-03 10:32:47 +0200

    Added existsByUsername and existsByEmail funcs

--- src/main/java/com/futurum/adcampaignmanager/repositories/UserRepository.java
+++ src/main/java/com/futurum/adcampaignmanager/repositories/UserRepository.java
@@ -1,9 +1,14 @@
 package com.futurum.adcampaignmanager.repositories;
 
 import com.futurum.adcampaignmanager.entity.User;
+import com.futurum.adcampaignmanager.enums.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+import static com.futurum.adcampaignmanager.enums.Role.USER;
 
 @Repository
 public interface UserRepository extends JpaRepository<User,Long> {
@@ -12,7 +17,15 @@ public interface UserRepository extends JpaRepository<User,Long> {
 
     User findByEmail(String email);
 
+    List<User> findByRole(Role role);
+
+    boolean existsByUsername(String username);
+
+    boolean existsByEmail(String email);
+
     @Query("SELECT COUNT(u) FROM User u WHERE u.role = 'USER'")
     long countUsersWithRoleUser();
+
+
 
 }
