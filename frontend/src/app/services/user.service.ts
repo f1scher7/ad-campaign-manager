938c211fe5d3763724e1a40922acf6c0efa59283
f1scher7 <maksym.szylo@gmail.com>
2024-07-03 10:35:49 +0200

    Added UserService

---
+++ frontend/src/app/services/user.service.ts
@@ -0,0 +1,54 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map, Observable } from 'rxjs';
+import { UserModel } from "../models/user.model";
+import { RoleEnum } from "../enums/role.enum";
+
+
+@Injectable({
+    providedIn: 'root'
+})
+export class UserService {
+
+    baseUrl: string = 'http://localhost:8080';
+    constructor(private http: HttpClient) { }
+
+    getAllUsers(): Observable<UserModel[]> {
+        return this.http.get<UserModel[]>(this.baseUrl + '/api/users/all').pipe(
+            map(users => {
+                return users.map(user => ({
+                    id: user.id,
+                    username: user.username,
+                    password: user.password,
+                    email: user.email,
+                    role: this.mapRole(user.role),
+                }));
+            })
+        );
+    }
+
+    createUser(user: UserModel): Observable<UserModel> {
+        return this.http.post<UserModel>(`${this.baseUrl}/api/users/create-user`, user, {
+            headers: { 'Content-Type': 'application/json' }
+        });
+    }
+
+    updateUser(user: UserModel): Observable<UserModel> {
+        return this.http.put<UserModel>(`${this.baseUrl}/api/users/update-user-by-id/${user.id}`, user);
+    }
+
+    deleteUser(user: UserModel): Observable<void> {
+        return this.http.delete<void>(`${this.baseUrl}/api/users/delete-user-by-id/${user.id}`);
+    }
+
+    private mapRole(role: string): RoleEnum {
+        switch (role) {
+            case 'USER':
+                return RoleEnum.USER;
+            case 'ADMIN':
+                return RoleEnum.ADMIN;
+            default:
+                return RoleEnum.USER;
+        }
+    }
+}
