82f2d553949c67410253c4775bbb5af7f764f698
f1scher7 <maksym.szylo@gmail.com>
2024-07-04 10:38:57 +0200

    Added routes for user-management and campaign-management

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,11 +3,17 @@ import { LoginComponent } from "./components/auth/login/login.component";
 import { RegisterComponent } from "./components/auth/register/register.component";
 import { AdminDashboardComponent } from "./components/admin/admin-dashboard/admin-dashboard.component";
 import { AuthGuard } from "./guards/auth.guard";
+import {UserManagementComponent} from "./components/admin/users/user-management/user-management.component";
+import {
+    CampaignManagementComponent
+} from "./components/admin/campaings/campaign-management/campaign-management.component";
 
 export const routes: Routes = [
     { path: '', redirectTo: 'auth/login', pathMatch: "full" },
     { path: 'auth/login', component: LoginComponent },
     { path: 'auth/register', component: RegisterComponent },
     { path: 'adashboard', component: AdminDashboardComponent, canActivate: [AuthGuard], data: { role: 'ADMIN' } },
+    { path: 'adashboard/user-management', component: UserManagementComponent, canActivate: [AuthGuard], data: { role: 'ADMIN' } },
+    { path: 'adashboard/campaign-management', component: CampaignManagementComponent, canActivate: [AuthGuard], data: { role: 'ADMIN' } },
     { path: '**', redirectTo: 'auth/login' },
 ];
