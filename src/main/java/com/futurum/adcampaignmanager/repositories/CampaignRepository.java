fdaa95c74f9e77adcbc8c69d90c144a7722302eb
f1scher7 <maksym.szylo@gmail.com>
2024-07-04 10:49:15 +0200

    Added some methods for CampaignRepository

--- src/main/java/com/futurum/adcampaignmanager/repositories/CampaignRepository.java
+++ src/main/java/com/futurum/adcampaignmanager/repositories/CampaignRepository.java
@@ -1,6 +1,7 @@
 package com.futurum.adcampaignmanager.repositories;
 
 import com.futurum.adcampaignmanager.entity.Campaign;
+import com.futurum.adcampaignmanager.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,5 +9,9 @@ import org.springframework.stereotype.Repository;
 public interface CampaignRepository extends JpaRepository<Campaign, Long> {
 
     Campaign findByCampaignName(String campaignName);
+
+    Campaign findByUser(User user);
+
+    boolean existsByCampaignName(String campaignName);
 
 }
